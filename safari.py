0dd1fdba9a22c0b4ca041de279773ccb54608ff6
2LargeFeet <michael.gombos@gmail.com>
2018-06-12 22:23:05 -0400

    works with top posts

--- safari.py
+++ safari.py
@@ -8,13 +8,5 @@ nations = [line.rstrip() for line in open('daily_list.txt','r')]
 
 for i in nations:
     subreddit = reddit.subreddit(i)
-    for submission in subreddit.top(limit=1):
-#        print("Title: ", submission.title)
-#        print("Text: ", submission.selftext)
-#        post = reddit.submission(submission.id)
-#         cross_post =
-#        for i in post:
-#        print(post.crosspost)
+    for submission in subreddit.hot(limit=1):
         reddit.submission(submission.id).crosspost(subreddit="redditsafari")
-#        post.crosspost(subreddit="redditsafari")
-#        print("---------------------------------\n")
